87f0947dbfc41b14b5e0071c1c18b3a1cd4c4e21
14Asif <asifalisayed19@gmail.com>
2023-05-31 20:58:24 +0530

    pushing for production

--- pages/contact.js
+++ pages/contact.js
@@ -91,7 +91,7 @@ function contact() {
                   name="email"
                   value={formData.email}
                   onChange={handleChange}
-                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                   placeholder="name@flowbite.com"
                   required
                 />

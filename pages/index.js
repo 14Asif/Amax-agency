03edc9e36ffa2fa021a3199961676f056c605c28
14Asif <asifalisayed19@gmail.com>
2023-06-06 19:51:48 +0530

    Adding updated code

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,33 @@ import Hero from "../components/Hero";
 import Layout from "../components/Layout/Layout";
 import SeoHead from "../components/SeoHead";
 import Service from "../components/Service";
-
+import { NextSeo } from "next-seo";
 export default function Home() {
   return (
     <>
-      <SeoHead title='AMAX Agency | Professional Website Design and Development Services' />
-      <Layout>
+        <NextSeo
+        title="AMAX Agency | Professional Website Design and Development Services"
+        description=  "AMAX Agency offers top-notch website design and development services tailored for SEO success. Our expert team creates visually stunning and user-friendly websites optimized for search engines. Boost your online visibility, engage visitors, and convert leads into customers with our cutting-edge web solutions. Partner with AMAX Agency to elevate your brand's online presence. Contact us today for exceptional design, seamless functionality, and maximum SEO performance."
+        keywords="Experienced online marketing professionals,Expertise in digital marketing,
+        Knowledgeable online marketing team,
+          Specialized in internet marketing strategies,
+          Proven track record in online advertising,
+          Extensive experience in SEO and SEM,
+          Social media marketing specialists,
+          Email marketing experts,
+          Content marketing strategists,
+          Results-driven online marketing approach,
+          Advanced analytics and data-driven insights,
+          Cutting-edge online marketing techniques,
+          Dedicated to driving online success,
+          Comprehensive understanding of online consumer behavior,
+          Online marketing industry leaders,
+          Passionate about delivering online marketing excellence,
+          Customer-focused online marketing solutions,
+          Constantly adapting to online marketing trends,
+          Trusted by clients for online marketing expertise `"
+      />
+        <Layout>
         <Hero />
         <Service />
         <Pricing />

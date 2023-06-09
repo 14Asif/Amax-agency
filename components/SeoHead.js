403c237f9b0a42a8bcf31915d43b89d6c2fcf448
14Asif <asifalisayed19@gmail.com>
2023-06-09 17:00:09 +0530

    Added Updated SEO keywords

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -3,31 +3,14 @@ import { useRouter } from "next/router";
 
 // Default value for some meta data
 const defaultMeta = {
-  title: "AMAX Agency | Professional Website Design and Development Services",
+  title: "Professional Website Design and Development Services | AMAX Agency",
   siteName: "AMAX Agency",
   description:
-    "AMAX Agency offers top-notch website design and development services tailored for SEO success. Our expert team creates visually stunning and user-friendly websites optimized for search engines. Boost your online visibility, engage visitors, and convert leads into customers with our cutting-edge web solutions. Partner with AMAX Agency to elevate your brand's online presence. Contact us today for exceptional design, seamless functionality, and maximum SEO performance.",
+    "AMAX Agency offers professional website design and development services tailored for SEO success. Our experienced team creates visually stunning, user-friendly websites optimized for search engines. Boost your online visibility, engage visitors, and convert leads into customers with our cutting-edge web solutions. Partner with AMAX Agency to elevate your brand's online presence. Contact us today for exceptional design, seamless functionality, and maximum SEO performance.",
   // change base url of your web (without '/' at the end)
   // url: 'https://next-landing-vpn.vercel.app',
   type: "website",
-  keywords: `Experienced online marketing professionals,Expertise in digital marketing,
-Knowledgeable online marketing team,
-  Specialized in internet marketing strategies,
-  Proven track record in online advertising,
-  Extensive experience in SEO and SEM,
-  Social media marketing specialists,
-  Email marketing experts,
-  Content marketing strategists,
-  Results-driven online marketing approach,
-  Advanced analytics and data-driven insights,
-  Cutting-edge online marketing techniques,
-  Dedicated to driving online success,
-  Comprehensive understanding of online consumer behavior,
-  Online marketing industry leaders,
-  Passionate about delivering online marketing excellence,
-  Customer-focused online marketing solutions,
-  Constantly adapting to online marketing trends,
-  Trusted by clients for online marketing expertise `,
+  keywords: `Professional website design and development services Tailored SEO strategies for online success Experienced online marketing professionals Expertise in digital marketing and internet marketing strategies Proven track record in online advertising, SEO, and SEM Social media marketing specialists driving engagement and growth Email marketing experts for effective customer communication Content marketing strategists creating compelling and SEO-friendly content Results-driven online marketing approach with advanced analytics Cutting-edge online marketing techniques for optimal performance Trusted industry leaders in online marketing solutions Dedicated to driving online success through comprehensive strategies Constantly adapting to online marketing trends and consumer behavior `,
   robots: "follow, index",
   // change with url of your image (recommended dimension = 1.91:1)
   // used in twitter, facebook, etc. card when link copied in tweet/status
--- pages/index.js
+++ pages/index.js
@@ -50,26 +50,9 @@ export default function Home() {
           <BreadcrumbData data={breadcrumbData}/>
       <OrganizationData data={organizationData} />
         <NextSeo
-        title="AMAX Agency | Professional Website Design and Development Services"
-        description=  "AMAX Agency offers top-notch website design and development services tailored for SEO success. Our expert team creates visually stunning and user-friendly websites optimized for search engines. Boost your online visibility, engage visitors, and convert leads into customers with our cutting-edge web solutions. Partner with AMAX Agency to elevate your brand's online presence. Contact us today for exceptional design, seamless functionality, and maximum SEO performance."
-        keywords="Experienced online marketing professionals,Expertise in digital marketing,
-        Knowledgeable online marketing team,
-          Specialized in internet marketing strategies,
-          Proven track record in online advertising,
-          Extensive experience in SEO and SEM,
-          Social media marketing specialists,
-          Email marketing experts,
-          Content marketing strategists,
-          Results-driven online marketing approach,
-          Advanced analytics and data-driven insights,
-          Cutting-edge online marketing techniques,
-          Dedicated to driving online success,
-          Comprehensive understanding of online consumer behavior,
-          Online marketing industry leaders,
-          Passionate about delivering online marketing excellence,
-          Customer-focused online marketing solutions,
-          Constantly adapting to online marketing trends,
-          Trusted by clients for online marketing expertise"
+        title="Professional Website Design and Development Services | AMAX Agency"
+        description=  "AMAX Agency offers professional website design and development services tailored for SEO success. Our experienced team creates visually stunning, user-friendly websites optimized for search engines. Boost your online visibility, engage visitors, and convert leads into customers with our cutting-edge web solutions. Partner with AMAX Agency to elevate your brand's online presence. Contact us today for exceptional design, seamless functionality, and maximum SEO performance."
+        keywords="Professional website design and development services Tailored SEO strategies for online success Experienced online marketing professionals Expertise in digital marketing and internet marketing strategies Proven track record in online advertising, SEO, and SEM Social media marketing specialists driving engagement and growth Email marketing experts for effective customer communication Content marketing strategists creating compelling and SEO-friendly content Results-driven online marketing approach with advanced analytics Cutting-edge online marketing techniques for optimal performance Trusted industry leaders in online marketing solutions Dedicated to driving online success through comprehensive strategies Constantly adapting to online marketing trends and consumer behavior"
       />
 
         <Layout>

4809c17a6c67dcf91926503665d021f74f9ef7af
Sahil-06 <sahil908286@gmail.com>
2023-10-08 12:54:51 +0530

    favicon and animation removed

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -112,6 +112,7 @@ const favicons = [
     rel: "apple-touch-icon",
     sizes: "57x57",
     href: "/favicon/apple-icon-57x57.png",
+   
   },
   {
     rel: "apple-touch-icon",
@@ -131,54 +132,54 @@ const favicons = [
   {
     rel: "apple-touch-icon",
     sizes: "114x114",
-    href: "/public/favicon/apple-icon-114x114.png",
+    href: "/favicon/apple-icon-114x114.png",
   },
   {
     rel: "apple-touch-icon",
     sizes: "120x120",
-    href: "/public/favicon/apple-icon-120x120.png",
+    href: "/favicon/apple-icon-120x120.png",
   },
   {
     rel: "apple-touch-icon",
     sizes: "144x144",
-    href: "/public/favicon/apple-icon-144x144.png",
+    href: "/favicon/apple-icon-144x144.png",
   },
   {
     rel: "apple-touch-icon",
     sizes: "152x152",
-    href: "/public/favicon/apple-icon-152x152.png",
+    href: "/favicon/apple-icon-152x152.png",
   },
   {
     rel: "apple-touch-icon",
     sizes: "180x180",
-    href: "/public/favicon/apple-icon-180x180.png",
+    href: "/favicon/apple-icon-180x180.png",
   },
   {
     rel: "mask-icon",
-    href: "/public/favicon/safari-pinned-tab.svg",
+    href: "/favicon/safari-pinned-tab.svg",
     color: "#F59A9A",
   },
   {
     rel: "icon",
-    href: "/public/favicon/favicon.ico",
+    href: "/favicon/favicon.ico",
   },
   {
     rel: "icon",
     type: "image/png",
     sizes: "16x16",
-    href: "/public/favicon/favicon-16x16.png",
+    href: "/favicon/favicon-16x16.png",
   },
   {
     rel: "icon",
     type: "image/png",
     sizes: "32x32",
-    href: "/public/favicon/favicon-32x32.png",
+    href: "/favicon/favicon-32x32.png",
   },
   {
     rel: "icon",
     type: "image/png",
     sizes: "96x96",
-    href: "/public/favicon/favicon-96x96.png",
+    href: "/favicon/favicon-96x96.png",
   },
 ];
 

e530b3393f6d23b549d75d19e7e7213cd4f8d431
14Asif <asifalisayed19@gmail.com>
2024-02-25 15:34:45 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    // Add more paths as needed for other files in your project
+  ],
   theme: {
     boxShadow: {
       sm: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
